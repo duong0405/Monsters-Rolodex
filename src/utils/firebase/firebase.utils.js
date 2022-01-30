04d5090fbf1d7f3b81198a928c687ac5647128fe
Phuong Duong <phuongx31@gmail.com>
2022-01-30 17:02:51 +0700

    setup databse with firestore

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app"
 import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from "firebase/auth"
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore"
 
 const firebaseConfig = {
     apiKey: "AIzaSyBnNMQ3YLS8X7GuH16uwITV9Zj5T-w1hl0",
@@ -20,3 +21,23 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocument = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid);
+    const userSnapshot = await getDoc(userDocRef);
+
+    if (!userSnapshot.exists()) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await setDoc(userDocRef, { displayName, email, createdAt });
+        } catch (e) {
+            console.log('error creating the user', e.message);
+        }
+    }
+
+    return userDocRef;
+}
