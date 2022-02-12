4d1cc7494456af42593c6010f19f9e8fa9309529
Phuong Duong <phuongx31@gmail.com>
2022-02-12 14:36:00 +0700

    add product card component and style

--- src/shop/shop.component.jsx
+++ src/shop/shop.component.jsx
@@ -1,13 +1,18 @@
 import { useContext } from "react";
 import { ProductsContext } from "../contexts/products.context";
+import ProductCard from "../components/products-card/product-card.component";
+
+import "./shop.styles.scss";
 
 const Shop = () => {
 
     const { products } = useContext(ProductsContext);
 
     return (
-        <div>
-            {products.map((item) => <h1>{item.name}</h1>)}
+        <div className="products-container">
+            {products.map((item) => (
+                <ProductCard key={item.id} product={item} />
+            ))}
         </div>
     )
 }
