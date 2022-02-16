9f500b66a4f48f5bc0565e6561e901aef9e8b2c9
Phuong Duong <phuongx31@gmail.com>
2022-02-16 15:38:14 +0700

    add cart dropdown component and cart context

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -4,9 +4,8 @@ import "./cart-dropdown.styles.scss";
 const CartDropdown = () => {
     return (
         <div className="cart-dropdown-container">
-            <div className="cart-items">
-                <Button>GO TO CHECKOUT</Button>
-            </div>
+            <div className="cart-items" />
+            <Button>GO TO CHECKOUT</Button>
         </div>
     )
 }
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -4,6 +4,7 @@ import { ReactComponent as Crown } from "../../assets/crown.svg"
 
 import CartIcon from "../../components/card-icon/cart-icon.component"
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component"
+import { CartContext } from "../../contexts/cart.context"
 
 import { UserContext } from "../../contexts/user.context"
 import { signOutUser } from "../../utils/firebase/firebase.utils"
@@ -12,6 +13,7 @@ import "./navigation.style.scss"
 
 const Navigation = () => {
     const { currentUser } = useContext(UserContext);
+    const { isCartOpen } = useContext(CartContext);
 
     return (
         <Fragment>
@@ -31,7 +33,7 @@ const Navigation = () => {
                     }
                     <CartIcon />
                 </div>
-                <CartDropdown />
+                {isCartOpen && <CartDropdown />}
             </div>
             <Outlet />
         </Fragment>
